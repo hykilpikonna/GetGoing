1bea68cf284047f055a94ed8d9975ed9a13d3c18
Hykilpikonna <me@hydev.org>
2021-01-08 16:14:42 -0500

    [+] Create backend spring project

---
+++ Backend/src/test/kotlin/org/hydev/ios/alarmclock/AlarmClockApplicationTests.kt
@@ -0,0 +1,15 @@
+package org.hydev.ios.alarmclock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlarmClockApplicationTests
+{
+
+    @Test
+    fun contextLoads()
+    {
+    }
+
+}
