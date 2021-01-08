8931d00dd9b4f5393943729053a82e4225a25ed2
Hykilpikonna <me@hydev.org>
2021-01-08 18:34:57 -0500

    [+] Implement wvm picker data source

--- ProjectClock/AddAlarmViewController.swift
+++ ProjectClock/AddAlarmViewController.swift
@@ -9,20 +9,46 @@ import UIKit
 
 class AddAlarmViewController: UIViewController
 {
+    // UI: Make scroll view scrollable
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var scrollViewInner: UIView!
-    
-    @IBOutlet weak var timePicker: UIDatePicker!
-    @IBOutlet weak var wvmPicker: UIPickerView!
-    
     override func viewDidLayoutSubviews()
     {
         scrollView.addSubview(scrollViewInner)
         scrollView.contentSize = scrollViewInner.frame.size
     }
     
+    // Pickers
+    @IBOutlet weak var timePicker: UIDatePicker!
+    @IBOutlet weak var wvmPicker: UIPickerView!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+    }
+}
+
+class WVMDataSource: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSource
+{
+    required init?(coder: NSCoder)
+    {
+        super.init(coder: coder)
+        delegate = self
+        dataSource = self
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    func pickerView(_ v: UIPickerView, numberOfRowsInComponent: Int) -> Int
+    {
+        return wvms.count
+    }
+    
+    func pickerView(_ v: UIPickerView, titleForRow r: Int, forComponent: Int) -> String?
+    {
+        return wvms[r].name + " - " + wvms[r].desc
     }
 }
