2b75c2361740a565bb5549b57dc47bc1d6e413b0
Hykilpikonna <me@hydev.org>
2021-01-08 18:34:07 -0500

    [+] Create wvm struct

---
+++ ProjectClock/Logic.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectClock
+//
+//  Created by Hykilpikonna on 1/8/21.
+//
+
+import Foundation
+
+struct WVM
+{
+    let name: String
+    let desc: String
+}
+
+let wvms = [
+    WVM(name: "Walk", desc: "Walk a few steps"),
+    WVM(name: "Jump", desc: "Make a few jumps"),
+    WVM(name: "Puzzle", desc: "Complete a simple puzzle"),
+    WVM(name: "Smash", desc: "It'll never truns off"),
+]
