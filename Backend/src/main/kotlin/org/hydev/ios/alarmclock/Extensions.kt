55d876952c33f0d9e9b7f3708e75b31c6ff4c770
Hykilpikonna <me@hydev.org>
2021-01-09 18:52:21 -0500

    [B][+] Create function to generate random salt

--- Backend/src/main/kotlin/org/hydev/ios/alarmclock/Extensions.kt
+++ Backend/src/main/kotlin/org/hydev/ios/alarmclock/Extensions.kt
@@ -1,5 +1,32 @@
 package org.hydev.ios.alarmclock
 
 import org.springframework.http.ResponseEntity
+import java.security.SecureRandom
+import javax.crypto.SecretKeyFactory
 
+import javax.crypto.spec.PBEKeySpec
+
+import java.security.spec.KeySpec
+import java.util.*
+
+/**
+ * Generate "Bad Request" response entity
+ *
+ * @param msg Message
+ * @return Response entity
+ */
 fun bad(msg: String): ResponseEntity<String> = ResponseEntity.badRequest().body(msg)
+
+/**
+ * Generate random salt
+ *
+ * @param len Length of the salt in bytes
+ * @return Random byte array of size len
+ */
+fun randSalt(len: Int = 16): ByteArray
+{
+    val random = SecureRandom()
+    val salt = ByteArray(16)
+    random.nextBytes(salt)
+    return salt
+}
