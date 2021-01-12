7017a73a9a5db57789f24dbad02b96d3299ffb99
Hykilpikonna <me@hydev.org>
2021-01-12 13:46:36 -0500

    [+]  Create user model

---
+++ ProjectClock/Models.swift
@@ -0,0 +1,16 @@
+//
+//  Models.swift
+//  ProjectClock
+//
+//  Created by Hykilpikonna on 1/12/21.
+//
+
+import Foundation
+
+struct User
+{
+    var id: Int
+    var name: String
+    var email: String
+    var pass: String
+}
