d01d56b6ef298a29101b5b196359919445e0354c
Hykilpikonna <me@hydev.org>
2021-01-17 16:18:56 -0500

    [O] Loop alarm sound + vibration

--- ProjectClock/AlarmActivationViewController.swift
+++ ProjectClock/AlarmActivationViewController.swift
@@ -10,9 +10,17 @@ import AVFoundation
 
 class AlarmActivationViewController: UIViewController
 {
+    var timer: Timer?
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(AlarmActivationViewController.playSound), userInfo: nil, repeats: true)
+    }
+    
+    @objc func playSound()
+    {
         AudioServicesPlayAlertSound(SystemSoundID(1005))
+        AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
     }
 }
