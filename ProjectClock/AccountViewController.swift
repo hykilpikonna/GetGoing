6f76ff90cd704de785c417a4d2a385c735a95fcd
Hykilpikonna <me@hydev.org>
2021-01-22 14:05:02 -0500

    [+] Create Login view controller

--- ProjectClock/AccountViewController.swift
+++ ProjectClock/AccountViewController.swift
@@ -9,8 +9,32 @@ import UIKit
 
 class AccountViewController: UIViewController
 {
+    @IBOutlet var vLogin: UIView!
+    @IBOutlet var vManage: UIView!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
     }
+}
+
+class LoginVC: UIViewController
+{
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func register(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func login(_ sender: Any)
+    {
+        
+    }
+}
+
+class ManageVC: UIViewController
+{
+    
 }
