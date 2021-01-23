e6817bd795a6fb649ad3a2f25784def9fa1764f7
Dallon Archibald <darchibald21@stjohnsprep.org>
2021-01-23 17:34:28 -0500

    Created Stopwatch controller and adjusted/linked tab bar

---
+++ ProjectClock/StopwatchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StopwatchViewController.swift
+//  ProjectClock
+//
+//  Created by Dallon Archibald on 1/23/21.
+//
+
+import UIKit
+
+class StopwatchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
